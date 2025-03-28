4b24fbe6cc5a07739597b85c5eb20a66776e04f8
fabian <cfacosta.16@gmail.com>
2025-03-27 21:48:48 -0300

    corrección🥇

---
+++ main.js
@@ -0,0 +1,30 @@
+//* Parte 2: Corrección de Código en JavaScript 
+
+/* function operar(a, b, callback) {
+    return callback(a, b);
+}
+
+function resta(x, y) {
+    x - y; 
+}
+
+let resultado = operar(15, "5", resta); 
+
+console.log("El resultado de la operación es:", resultado);
+ 'El resultado de la operación es:' 10 */
+
+(
+ Ejercicio Realizado con una correcion
+
+ function operar (a,b, callback) {
+    return callback(a, b);
+ }
+
+ function resta(x,y) {
+    return x -y;
+ }
+ let resultado = operar(15,("5"), resta);
+ console.log("El resultado de la operacion es:", resultado);
+ // el resultado de la operacion es: 10
+
+ 
